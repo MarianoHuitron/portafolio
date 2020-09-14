ec7dcabe7a015a3b8569aeb35739494b2738cf76
Mariano <mariano.14th@gmail.com>
2020-09-14 10:57:08 -0600

    Updates

---
+++ precache-manifest.ab7d98ae055d738d5f8c032dc589ac55.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1896626035f7c74c08d6836bf77b0132",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "477fae1953e27046bd24",
+    "url": "/portafolio/static/css/2.ea474690.chunk.css"
+  },
+  {
+    "revision": "538110a70f82c0fca630",
+    "url": "/portafolio/static/css/main.97a87a24.chunk.css"
+  },
+  {
+    "revision": "477fae1953e27046bd24",
+    "url": "/portafolio/static/js/2.e4e5bac9.chunk.js"
+  },
+  {
+    "revision": "74eff66043ffe02b40eae6b24ef54ab2",
+    "url": "/portafolio/static/js/2.e4e5bac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538110a70f82c0fca630",
+    "url": "/portafolio/static/js/main.3b043a40.chunk.js"
+  },
+  {
+    "revision": "11f9e52ab9233dae687b",
+    "url": "/portafolio/static/js/runtime-main.6d304cc1.js"
+  },
+  {
+    "revision": "a9c8a9f3e60aca29f604485a1c71ea88",
+    "url": "/portafolio/static/media/MarianoResume.a9c8a9f3.pdf"
+  },
+  {
+    "revision": "fbcd67c9217544a9da7bbb12b202d45a",
+    "url": "/portafolio/static/media/cards.fbcd67c9.png"
+  },
+  {
+    "revision": "3c314814d665c3618f90b9eebf360414",
+    "url": "/portafolio/static/media/flydata.3c314814.png"
+  },
+  {
+    "revision": "c2ddbc2394700972e578d07ba8af508d",
+    "url": "/portafolio/static/media/flydatadesk.c2ddbc23.png"
+  },
+  {
+    "revision": "0032f4b80e4804ed96275b71e84b0b07",
+    "url": "/portafolio/static/media/flydatamobile.0032f4b8.png"
+  },
+  {
+    "revision": "2210dd36fcf07ea334c3c9aa583fab7c",
+    "url": "/portafolio/static/media/imagen.2210dd36.jpg"
+  },
+  {
+    "revision": "5710cc11ff6a1ed708462fdf2b0f6d7d",
+    "url": "/portafolio/static/media/pedidosApp1.5710cc11.png"
+  },
+  {
+    "revision": "90f40bed654e53c3c98dfd060c9bf31c",
+    "url": "/portafolio/static/media/pedidosApp2.90f40bed.png"
+  },
+  {
+    "revision": "2679928054b0d217e34ea0b65d73e053",
+    "url": "/portafolio/static/media/rutaz-app-2.26799280.jpg"
+  },
+  {
+    "revision": "4dc4682175dc10bc8448d33279e46a42",
+    "url": "/portafolio/static/media/rutaz-app-3.4dc46821.jpg"
+  },
+  {
+    "revision": "671de9cf212ba8f068c4ce2ce70dc6fa",
+    "url": "/portafolio/static/media/rutazApp.671de9cf.png"
+  },
+  {
+    "revision": "a208f5dbf44107117f443a131950952b",
+    "url": "/portafolio/static/media/sisAdmin1.a208f5db.png"
+  },
+  {
+    "revision": "29775b6431c35a313d29668bf65632b3",
+    "url": "/portafolio/static/media/sisAdmin2.29775b64.png"
+  }
+]);
